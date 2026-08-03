44a2e4224eab02c153bbd7bb492739899d734446
Daniyal Saqib <daniyalsaqib04@gmail.com>
2026-08-03 17:22:30 +0500

    logging off 03/08/26

--- date.cpp
+++ date.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+/* code -r .
+Basically here if current date: 03/08/2026
+then after +1000 days date should be: 29/08/2029
+(IT'S BASICALLY A TEST CASE NOW BRAINSTORM LOGIC 4:39 PM 03/08/2026)
+
+*/
+
 int main()
 {
     
