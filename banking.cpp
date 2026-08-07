9737fa641d24ef27a84fee33667273ebf73e7f4b
Daniyal Saqib <daniyalsaqib04@gmail.com>
2026-08-07 11:57:14 +0500

    gave viva to sir affan of calculator

--- banking.cpp
+++ banking.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip> // for formatting purposes setw() etc
 using namespace std;
 
 /*
@@ -6,6 +7,7 @@ BRAINSTORMING THREAD
 under sir affan waheed
 methods and attributes in OOP
 for clear screen use system("cls")
+getter and setters
 */
 
 // we'll use Object Oriented Programming here 
@@ -18,11 +20,11 @@ int accountnumber, balance; // i'm using balance as integer for simplifying purp
 
 public:
 // CUSTOMERS DETAILS
-void details() {
-cout << "ALLIED BANK CUSTOMER DETAILS " << endl;
-cout << "Name : " << name << endl;
-cout << "Account Number : " << accountnumber << endl;
-cout << "Balance : " << balance << endl;
+int getBalance() {
+return balance;
+}
+void Deposit() {
+    
 }
 
 };
@@ -32,9 +34,11 @@ int main()
     int choice; // for menu based system input
 
     do {
-    cout << "WELCOME TO ALLIED BANK! " << endl;
-    cout << " WHAT WOULD YOU LIKE TO DO? " << endl;
-    cout << " 0. EXIT" << endl;
+    cout << "============================== " << endl;
+    cout << setw(5) << "ALLIED BANK - MAIN MENU" << endl;
+    cout << "============================== " << endl;
+    // cout << "Currently selected account: [" <<
+    cout << "0. EXIT" << endl;
     cout << " 1. Access Account 1" << endl;
     cout << " 2. Access Account 2" << endl;
     cout << " 3. Access Account 3" << endl;
