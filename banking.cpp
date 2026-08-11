2d6a5f33fac69e53b688ac2aff3ccbbaa2ceb5f9
Daniyal Saqib <daniyalsaqib04@gmail.com>
2026-08-11 10:30:41 +0500

    main menu formatting done

--- banking.cpp
+++ banking.cpp
@@ -33,20 +33,26 @@ int main()
 {
     int choice; // for menu based system input
 
-    do {
+    system("cls"); // for clearing the directory line
+
+    do { // 25 to 30 == 26 perfect value
     cout << "============================== " << endl;
-    cout << setw(5) << "ALLIED BANK - MAIN MENU" << endl;
+    cout << setw(26) << "ALLIED BANK - MAIN MENU" << endl;
     cout << "============================== " << endl;
-    // cout << "Currently selected account: [" <<
-    cout << "0. EXIT" << endl;
-    cout << " 1. Access Account 1" << endl;
-    cout << " 2. Access Account 2" << endl;
-    cout << " 3. Access Account 3" << endl;
-    cout << " 4. Access Account 4" << endl; 
-    cout << " 5. Clear Screen" << endl; // system("cls")
+    cout << "Currently selected account: [" << 01035513201 << " - " << "owner name" << "]" << endl;
+    cout << endl; // VERTICAL SPACING AS per the requirements. 
+    
+    cout << "0. Clear Screen " << endl; // added feature for clearing output
+    cout << "1. Select Account" << endl;
+    cout << "2. Check Balance" << endl;
+    cout << "3. Deposit" << endl;
+    cout << "4. Withdraw" << endl; 
+    cout << "5. Exit" << endl; // system("cls")
+    cout << "============================== " << endl;
+    cout << "Enter your choice: ";
     cin >> choice;
 
-    if (choice == 5) {system("cls");}
-    } while (choice != 0);
+    if (choice == 0) {system("cls");}
+    } while (choice != 5); // 5 enter krne pr exit hai
     return 0;
 }
